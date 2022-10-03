8a5921fc72b622747e83483513e7e871512752d8
Thanga Ganapathy <ganapathy888@gmail.com>
2022-10-03 12:49:08 +0530

    Init with nextjs

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Tharavugal.org</title>
+        <meta
+          name="description"
+          content="Real-time events are structured and can be used for analysis and research purposes."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Coming soon...</h1>
+      </main>
+
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
