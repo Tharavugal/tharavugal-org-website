24ae2c2025422307f32713136be29d85307826a7
Thanga Ganapathy <ganapathy888@gmail.com>
2023-02-05 20:32:23 +0530

    Upgrade next 12 to 13

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Coming soon...</h1>
+        <h1 className={styles.title}>விரைவில்...</h1>
       </main>
 
       <footer className={styles.footer}></footer>
