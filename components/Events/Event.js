5ae89eef87a481d423a637fdd72d2f28fd88c490
Thanga Ganapathy <ganapathy888@gmail.com>
2023-09-21 02:00:34 +0530

    Add event categories to the list

--- components/Events/Event.js
+++ components/Events/Event.js
@@ -31,6 +31,18 @@ export default function Event({ data }) {
             />
           ))}
         </Box>
+        <Box sx={{ mt: 2 }}>
+          {data.categories.map((c, i) => (
+            <Chip
+              variant="outlined"
+              color="info"
+              key={i}
+              label={c}
+              sx={{ mt: { xs: 1 }, mr: '2px' }}
+              size="small"
+            />
+          ))}
+        </Box>
         <Box mt={1} sx={{ textAlign: 'right' }}>
           <Typography variant="span" fontSize={12} color="text.secondary">
             {format(new Date(data.startedAt), 'yyyy-MM-dd hh:mm:ss aa')}{' '}
