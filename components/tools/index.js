3b8ce7519c8bfa419c0ee9a106a433d1c01eec79
Thanga Ganapathy <ganapathy888@gmail.com>
2023-10-23 00:15:10 +0530

    Categorieze the tools in the home page

--- components/tools/index.js
+++ components/tools/index.js
@@ -8,6 +8,7 @@ import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
 import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import FastfoodIcon from '@mui/icons-material/Fastfood';
+import WaterDropOutlinedIcon from '@mui/icons-material/WaterDropOutlined';
 
 import ToolBox from './ToolBox';
 
@@ -18,6 +19,15 @@ export default function Tools() {
         <ChevronRightIcon /> Tools
       </Typography>
       <Divider />
+
+      <Typography
+        variant="subtitle1"
+        sx={{ display: 'flex', alignItems: 'center', mt: 2 }}
+      >
+        Primary
+      </Typography>
+      <Divider />
+
       <Box
         py={2}
         sx={{ display: 'flex', justifyContent: 'flex-start', flexWrap: 'wrap' }}
@@ -28,11 +38,38 @@ export default function Tools() {
           label="Visualizer"
           path="/visualizer"
         />
+        <ToolBox icon={ScienceOutlinedIcon} label="Research" path="/research" />
+      </Box>
+      <Typography
+        variant="subtitle1"
+        sx={{ display: 'flex', alignItems: 'center', mt: 2 }}
+      >
+        Health
+      </Typography>
+      <Divider />
+
+      <Box
+        py={2}
+        sx={{ display: 'flex', justifyContent: 'flex-start', flexWrap: 'wrap' }}
+      >
         <ToolBox
           icon={FastfoodIcon}
           label="Food Ingredients"
           path="/food-ingredients"
         />
+      </Box>
+      <Typography
+        variant="subtitle1"
+        sx={{ display: 'flex', alignItems: 'center' }}
+      >
+        Thamizhl
+      </Typography>
+      <Divider />
+
+      <Box
+        py={2}
+        sx={{ display: 'flex', justifyContent: 'flex-start', flexWrap: 'wrap' }}
+      >
         <ToolBox
           icon={ImportContactsIcon}
           label="Thamizhl Dictionary"
@@ -43,16 +80,46 @@ export default function Tools() {
           label="Thirukkural"
           path="/thirukkural"
         />
-        <ToolBox icon={ScienceOutlinedIcon} label="Research" path="/research" />
+        <ToolBox
+          icon={CalendarMonthOutlinedIcon}
+          label="Thamizhl Calendar"
+          path="/thamizhl-calendar"
+        />
+      </Box>
+      <Typography
+        variant="subtitle1"
+        sx={{ display: 'flex', alignItems: 'center' }}
+      >
+        Others
+      </Typography>
+      <Divider />
+
+      <Box
+        py={2}
+        sx={{ display: 'flex', justifyContent: 'flex-start', flexWrap: 'wrap' }}
+      >
         <ToolBox
           icon={PushPinOutlinedIcon}
           label="On This Day"
           path="/on-this-day"
         />
+      </Box>
+      <Typography
+        variant="subtitle1"
+        sx={{ display: 'flex', alignItems: 'center' }}
+      >
+        Environment
+      </Typography>
+      <Divider />
+
+      <Box
+        py={2}
+        sx={{ display: 'flex', justifyContent: 'flex-start', flexWrap: 'wrap' }}
+      >
         <ToolBox
-          icon={CalendarMonthOutlinedIcon}
-          label="Thamizhl Calendar"
-          path="/thamizhl-calendar"
+          icon={WaterDropOutlinedIcon}
+          label="Water Bodies"
+          path="/water-bodies"
         />
       </Box>
     </Box>
